17115b73ef4547453ec42077dfd03dbf08363cff
aschillerstrom <33271281+aschillerstrom@users.noreply.github.com>
2018-04-06 22:31:38 -0500

    Create keys.js

---
+++ keys.js
@@ -0,0 +1,10 @@
+//console.log('you got the keys');
+
+
+exports.twitterKeys= {
+
+    consumer_key: 'SMtvdRreSKSC9HI0xFfYLNRLi',
+    consumer_secret: 'aiJh8JWC01UUze6JS9qcWuIXTydPjgB82uhzvjwpd6vYg9llpR',
+    access_token_key: '740143342550417412-oalTFsl2ixNqhcOeIUcuyAwNgyoKeom',
+    access_token_secret: 'ht9apDre9LjLRRQIH73mp6sUyf4eL7vj7Zfxt8xEPQLL5'
+};
